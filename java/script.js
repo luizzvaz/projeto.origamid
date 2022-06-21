cf0654849e3cf760cc0505808bd6777f1c21cff8
luizzvaz <104530945+luizzvaz@users.noreply.github.com>
2022-06-21 15:36:52 -0300

    js

---
+++ java/script.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+  const url= location.href
+  const href = link.href
+  if(url.includes(href)){
+    console.log(link)
+    link.classList.add("ativo")
+  }
+
+};
+
+links.forEach(ativarLink);
+
